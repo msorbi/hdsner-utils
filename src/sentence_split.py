e2813dd2550abfd417b1dd33e1cdb661d3ff18e6
Marco Sorbi <Marco.Sorbi@unige.ch>
2025-06-10 17:27:10 +0200

    sequence split on multiple tokens

--- src/sentence_split.py
+++ src/sentence_split.py
@@ -14,8 +14,8 @@ def parse_args():
     parser.add_argument("--split", type=str, required=True, help="dataset split (train|eval|test)")
     parser.add_argument("--classes", type=str, required=True, help="classes to consider, comma separated range including extremes, 1-based")
     parser.add_argument("--dictionary-size", type=int, default=10, help="size of dictionary sample to export, in percentage")
-    parser.add_argument("--max-seq-length", type=int, default=512, help="maximum output sequence length, notes: splits occur only on end-token, only tokens with label O are used as end")
-    parser.add_argument("--seq-end-token", type=str, default=".", help="end sequence token, default \".\"")
+    parser.add_argument("--max-seq-length", type=int, default=512, help="maximum output sequence length, notes: splits occur only on end-tokens")
+    parser.add_argument("--seq-end-tokens", type=str, default=".|;|,|Et|et|Le", help="end sequence tokens, case-sensitive, separated by |")
     parser.add_argument("--field-delimiter", type=str, default="\t", help="field delimiter, default TAB")
     parser.add_argument("--seed", type=int, default=42, help="random seed for dictionary sampling")
     args = parser.parse_args()
@@ -46,21 +46,18 @@ def sample(dictionary:list, percentage:int, seed:int) -> list:
     return sorted(random.sample(dictionary, size))
 
 def filter_classes(data, l, r):
-    # if l == r:
-    #     return [[x[0],x[l]] if len(x)>1 else [x[0]] for x in data] # TODO: fix
     output = []
     sent = []
     tokens = 0
     sents = 0
+    max_sent_len,msls = 0,0
     priors = {}
     for x in data:
         if len(x) <= 1:
-            # t = [[]]*2
-            # for i in range(2):
-            #     t[i] = [y[i+1] for y in sent]
             t = [[y[i] for y in sent] for i in range(l, r+1)]
             merged_labels = t[0] if l==r else merge_iob_labels(*t)
             tokens += len(sent)
+            max_sent_len,msls = max((max_sent_len,msls), (len(sent),len(output)))
             for y,z in zip(sent, merged_labels):
                 output.append([y[0],z])
                 if '-' in z:
@@ -75,17 +72,13 @@ def filter_classes(data, l, r):
         priors[k] /= tokens
     stats = {
         "tokens": tokens,
-        "sents": sents
+        "sents": sents,
+        "max_sent_len": max_sent_len,
+        "msls": msls
     }
     return output, stats, priors
 
-
-def main():
-    args = parse_args()
-    range_l, range_r = tuple(int(x) for x in args.classes.split(','))
-    with open(args.input, "r") as fp:
-        data = fp.readlines()
-    data += ["--\n"] # placeholder
+def split_sentences(data:list, seq_end_token:str, categories:list, max_seq_length:int) -> list:
     data_new = []
     sent = []
     categories = ["pers", "loc"]
@@ -94,27 +87,27 @@ def main():
     l = 0
     t = 0
     for i,row in enumerate(data):
-        if row == "--\n":
-            continue
-        t += 1
-        sent.append(row.strip().split(args.field_delimiter))
-        if row.startswith(args.field_delimiter.join([args.seq_end_token,"O","O",""])) and data[i+1][0].isupper():
-            if l + t <= args.max_seq_length:
-                l += t
-            else:
-                if l > 0:
-                    data_new.append([""]) # start new sequence
-                l = t
-            t = 0
-            data_new.extend(sent)
-            sent = []
-        if args.field_delimiter not in row:
+        sent.append(row)
+        if len(row) <= 1:
+            if l>0 and l+t > max_seq_length:
+                data_new.append([""])
             l = 0
             t = 0
             data_new.extend(sent)
             sent = []
         else:
-            fields = row.split(args.field_delimiter)
+            t += 1
+            if row[0] == seq_end_token:
+                if l + t <= max_seq_length:
+                    l += t
+                else:
+                    if l > 0:
+                        data_new.append([""]) # start new sequence
+                    l = t
+                t = 0
+                data_new.extend(sent)
+                sent = []
+            fields = row
             for f in fields[1:3]:
                 type_ = f[2:].lower()
                 if f.startswith("B-"):
@@ -125,9 +118,22 @@ def main():
                     entries[type_] = [fields[0]]
                 elif f.startswith("I-"):
                     entries[type_].append(fields[0])
-                
     data_new.extend(sent)
-    data_new, stats, priors = filter_classes(data_new, range_l, range_r)
+    return data_new, dictionary, entries
+
+def main():
+    args = parse_args()
+    range_l, range_r = tuple(int(x) for x in args.classes.split(','))
+    with open(args.input, "r") as fp:
+        data = fp.readlines()
+    data = [row.strip().split(args.field_delimiter) for row in data if row != "--\n"]
+    categories = ["pers", "loc"]
+    
+    for seq_end_token in args.seq_end_tokens.split("|"):
+        data, dictionary, entries = split_sentences(data, seq_end_token, categories, args.max_seq_length)
+
+                
+    data_new, stats, priors = filter_classes(data, range_l, range_r)
     data_new = "\n".join(args.field_delimiter.join(x) for x in data_new)
     with open(os.path.join(args.output_dir, f"{args.split}.txt"), "w") as fp:
         fp.write(data_new)
