3f4f7874c1c637eb53ca7768b5a050b8f33ee17f
Marco Sorbi <Marco.Sorbi@unige.ch>
2025-07-08 12:16:14 +0200

    evaluation summary script

---
+++ src/eval_summary.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# summarize evaluation outputs
+# evaluations are provided as filenames to the standard input, one per line
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", type=str, default="../data/hdsner_report.json", help="output JSON file")
+    args = parser.parse_args()
+    return args
+
+def main():
+    import sys
+    import json
+    args = parse_args()
+    summary = {}
+    for f in sys.stdin:
+        with open(f.strip(), 'r') as fp:
+            x = json.load(fp)
+        summary[f.strip().split('/')[-2]] = x
+    with open(args.output, 'w') as fp:
+        json.dump(obj=summary, fp=fp)
+
+if __name__ == "__main__":
+    main()
